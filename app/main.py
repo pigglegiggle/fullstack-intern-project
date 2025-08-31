f749eaee2cc692935a9fc4abce676ce922028083
pigglegiggle <sinnysinza47@gmail.com>
2025-08-31 23:55:06 +0700

    Add Dockerfile and things

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return "<h1>Hello World</h1>"
 
 @app.post("/ingest")
 async def ingest(request: Request):
--- app/send-data/script.py
+++ app/send-data/script.py
@@ -3,7 +3,7 @@ import socket
 server = "127.0.0.1"
 port = 514
 
-with open("../../logs/network.log", "r") as f:
+with open("../../logs/firewall.log", "r") as f:
     for line in f:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(line.strip().encode("ascii"), (server, port))
